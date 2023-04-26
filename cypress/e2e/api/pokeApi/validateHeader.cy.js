370ca32de028d4d1935f814eb854269d160c8210
Shyy88 <aishyash.sh@gmail.com>
2023-04-26 18:40:53 +0700

    validate header

---
+++ cypress/e2e/api/pokeApi/validateHeader.cy.js
@@ -0,0 +1,12 @@
+describe('Validate Header', () => {
+    it('Succesfully validate header content-type', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+        cy.get('@pokemon').its('headers').its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+        cy.get('@pokemon').its('body').should('have.property', 'forms')
+        cy.get('@pokemon').its('body').should('have.property', 'base_experience', 101)
+        
+    });
+})
+
+
