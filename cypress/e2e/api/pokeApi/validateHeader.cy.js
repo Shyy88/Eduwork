1955cf130cf9f9542548a9f70e68c2d3be1f8dfe
Shyy88 <aishyash.sh@gmail.com>
2023-04-27 12:05:55 +0700

    validate content

--- cypress/e2e/api/pokeApi/validateHeader.cy.js
+++ cypress/e2e/api/pokeApi/validateHeader.cy.js
@@ -1,4 +1,7 @@
-describe('Validate Header', () => {
+/// <reference types="cypress" />
+
+describe('Automation API with PokeAPI', () => {
+    
     it('Succesfully validate header content-type', () => {
         cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
         cy.get('@pokemon').its('headers').its('content-type')
@@ -6,6 +9,18 @@ describe('Validate Header', () => {
         cy.get('@pokemon').its('body').should('have.property', 'forms')
         cy.get('@pokemon').its('body').should('have.property', 'base_experience', 101)
         
+    });
+    
+    it('Succesfully validate status code', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('ditto')
+        cy.get('@ditto').its('status').should('equal', 200)
+    });
+// sangat penting untuk validate status karna memberitahu kita informasi tentang permintaan yang dibuat oleh kita untuk server. bila gagal kita dapat mengetahui mengapa bisa gagal.
+    it.only('Succesfully validate content', () => {
+    cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('ditto')
+        cy.get('@ditto').then((response) => {
+            expect(response.body.ability).to.eq(undefined)
+    })
     });
 })
 
